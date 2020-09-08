4936d80368881cc65ccf2e1d21990869ccd2a437
ying <kjwlatte@gmail.com>
2020-09-08 20:28:53 +0800

    20200908_新增ViewCart

---
+++ Fotricle/Models/ViewCart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fotricle.Models
+{
+    public class ViewCart
+    {
+        [Display(Name = "產品列表")]
+        public int ProductListId { get; set; }
+
+
+        [Display(Name = "產品數量")]
+
+        public int ProductUnit { set; get; }
+    }
+}
